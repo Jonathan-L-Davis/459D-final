c533af8894c31110d51e976c81b5903d3661d901
Jim-Boba <jldavisjr@yahoo.com>
2023-11-15 11:26:15 -0500

    AlU changes, not testable, but close

--- src/ALU.sv
+++ src/ALU.sv
@@ -1,34 +1,33 @@
-module smallALU( input bit [15:0] R[3:0], input bit [1:0] select1_reg, input bit [1:0] select2_reg, input bit sig, input bit op,  
-    output bit [15:0] out1, output bit [15:0] out2, output bit add_result_flags);
+module smallALU( input bit [15:0] R[3:0], 
+    input bit [1:0] select1_reg, input bit [1:0] select2_reg, 
+    input bit sig, input bit op,
+    output bit [7:0] out);
 
 logic [15:0] selReg[1:0];
 logic [15:0] midReg[1:0];
 
 
-logic [16:0] uadd_result;
-logic [31:0] umul_result;
-logic signed [15:0] sadd_result;
-logic signed [31:0] smul_result;
+logic [7:0] add_result;
+logic [7:0] sub_result;
+logic [7:0] and_result;
+logic [7:0]  or_result;
+logic [7:0] slt_result;
 
 
 assign selReg[0] = R[select1_reg];;
 assign selReg[1] = R[select2_reg];
 
-assign uadd_result = selReg[0]+selReg[1];
-assign umul_result = selReg[0]*selReg[1];
-assign sadd_result = $signed(selReg[0])+$signed(selReg[1]);
-assign smul_result = $signed(selReg[0])*$signed(selReg[1]);
+assign add_result = selReg[0]+selReg[1];
+assign sub_result = selReg[0]-selReg[1];
+assign  or_result = selReg[0]|selReg[1];
+assign and_result = selReg[0]&selReg[1];
+assign slt_result = selReg[0]<selReg[1];
 
-always @* begin
-  if(sig) begin
-    add_result_flags = (selReg[0][15]==selReg[1][15])&(selReg[0][15]!=sadd_result[15]);
-  end else begin
-    add_result_flags = uadd_result[16];
-  end
 
+always @* begin//used to update ALU flags, removed flags for now.
 end
 
-always @* begin
+always @* begin/*
     if(op) begin
         if(sig) begin
           out1 = smul_result[15:0];
@@ -43,7 +42,7 @@ always @* begin
       end else begin
         out1 = uadd_result[15:0];
       end
-    end
+    end//*/
 end
 
 
