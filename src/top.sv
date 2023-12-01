fd887ff056b1a9d02e21aa18e43d2780e80d4570
nddaet <142270307+nddaet@users.noreply.github.com>
2023-12-01 14:03:10 -0500

    Creates top module

---
+++ src/top.sv
@@ -0,0 +1,11 @@
+module top(
+    input clk_100MHz_main, input isMealy_main, 
+    input rst_main, input ctrl_input_main, 
+    output reg clock_led,
+    input [1:0] SW_input_main, 
+    output [3:0] Anode_Activate_main,
+    output [6:0] LED_out_main    
+);
+
+
+endmodule
