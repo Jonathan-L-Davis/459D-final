d0d842d369b8429b03ed35d3b1be16a40b43e2cb
Jim-Boba <jldavisjr@yahoo.com>
2023-12-01 21:57:50 -0500

    Bus almost complete, just need to iron out token system.

--- src/bus.sv
+++ src/bus.sv
@@ -20,21 +20,54 @@ module bus(
     output bit [7:0] RAM_data_in,
     input bit  [7:0] RAM_data_out,
     output bit rw
-    //GPIO interface
     
 );
     
     bit [1:0] in_progress;
+    bit [1:0] grant_given;
+    bit patron;
+    parameter wait_state = 0;
+    parameter write_state = 1;
+    parameter read_request_state = 2;
+    parameter read_deliver_state = 3;
+    bit [1:0] state;
     
     always @ ( posedge clk ) begin
         if( reset ) begin
             //reset important control signals, ignore rest
-           core0_grant <= 0;
-           core1_grant <= 0;
-           rw <= 0;
+            core0_grant <= 0;
+            core1_grant <= 0;
+            rw <= 0;
         end else if( core0_request && !in_progress[1] && !in_progress[0]) begin
-            // if nothing is in progress, start a request from 0, otherwise ignore and continue
-            //will change drastically
+            case(state)
+            wait_state: begin
+                // do tokening system
+                //  this is where patron variable is set
+                if( req[patron] ) begin
+                    state <= req_type[patron]+1;
+                end else if( req[~patron] )
+                    
+                end
+            end
+            write_state: begin
+                RAM_data_in <= data_in[patron];
+                RAM_address <= addr[patron];
+                rw <= 1;
+                state <= wait_state;
+            end
+            read_request_state: begin
+                RAM_address <= addr[patron];
+                rw <= 0;
+                state <= read_deliver_state;
+            end
+            read_deliver_state: begin
+                data_out <= RAM_data_out;
+                state <= wait_state;
+            end
+            endcase
         end
     end
+    
+    
+    
 endmodule
