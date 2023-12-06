be2a08dcd0aebe09dc8de6a97fb39875341af3cc
nddaet <142270307+nddaet@users.noreply.github.com>
2023-12-05 19:46:57 -0500

    Specified address widths

--- src/bus.sv
+++ src/bus.sv
@@ -8,7 +8,7 @@ module bus(
     output bit core0_grant,
     input bit [7:0] core0_data_in,
     output bit [7:0] core0_data_out,
-    input bit core0_address,
+    input bit [8:0] core0_address,
     
     //core 1 interface
     input bit core1_request,
@@ -16,7 +16,7 @@ module bus(
     output bit core1_grant,
     input bit [7:0] core1_data_in,
     output bit [7:0] core1_data_out,
-    input bit core1_address,
+    input bit [8:0] core1_address,
     
     //RAM interface
     output bit [8:0] RAM_address,
