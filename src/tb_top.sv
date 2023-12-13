26f180ae3dd367816f1fd868fadea8ae41adf9c5
nddaet <142270307+nddaet@users.noreply.github.com>
2023-12-13 17:40:59 -0500

    testbench revision

--- src/tb_top.sv
+++ src/tb_top.sv
@@ -1,6 +1,3 @@
-
-`timescale 1ns/1ps
-
 typedef enum {RTYPE, ITYPE, JTYPE} type_e;
 parameter ADDR_SPACE = 512;
 class RandInstructions;
@@ -55,23 +52,29 @@ class RandInstructions;
             funct == 6'b100000;
         }
     }
+
     constraint c_rsrt{
-            if(instruction_type == RTYPE){ {rs, rt, rd} inside {[5'b00000:5'b11111]};
-            }else if(instruction_type == ITYPE){ {rs, rt} inside {[5'b00000:5'b11111]};
-            }else{ {rs, rt, rd} inside {[5'b00000:5'b11111]};}
+            if(instruction_type == RTYPE){ 
+                rs inside {[5'b00000:5'b11111]};
+                rt inside {[5'b00000:5'b11111]};
+                rd inside {[5'b00000:5'b11111]};
+            }else if(instruction_type == ITYPE){ 
+                rs inside {[5'b00000:5'b11111]};
+                rt inside {[5'b00000:5'b11111]};
+            }else{ 
+                rs inside {[5'b00000:5'b11111]};
+                rt inside {[5'b00000:5'b11111]};
+                rd inside {[5'b00000:5'b11111]};
+            }
     }
 
     constraint c_immediate{
         if (instruction_type == ITYPE){
             if((instruction_number >=0 || instruction_number <=31) && is_store_instruction){
-                immediate_addr inside {[4'h0000:4'h007C], [4'h00FC:4'hFFFF]};
-            }else if((instruction_number >=32 || instruction_number <=63) && is_store_instruction){
-                immediate_addr inside {[4'h0080:4'hFFFF]};
-            }else{
-                immediate_addr inside {[4'h0000:4'hFFFF]};
+                immediate_addr inside {[16'h0000:16'hFFFF]};
             }
         }else{
-            immediate_addr == 4'h0000;
+            immediate_addr == 16'h0000;
         }
     }
 
@@ -100,50 +103,8 @@ class RandTransactions;
             foreach (trans_array[i]) begin
                 trans_array[i] = new();
             end
-        endfunction
-endclass
-
-
-class instruction_transitions;
-    int core_id;
-    bit [31:0] last_inst, curr_inst; // New variables
-    function new(int id);
-        core_id = id;
-    endfunction
-
-    covergroup opcodes;
-        option.per_instance = 1;
-
-        inst_reg_opcodes: coverpoint (core_id == 0 ? mips_inst.core0.IR : mips_inst.core1.IR) {
-        //coverpoint current_inst {
-            bins add = {32'b000000????????????????????100000};
-            bins sub = {32'b000000????????????????????100010};
-            bins and_instr = {32'b000000????????????????????100100};
-            bins or_instr = {32'b000000????????????????????100101};
-            bins slt = {32'b000000????????????????????101010};
-
-            bins addi = {32'b001000????????????????????????????};
-            bins beq = {32'b000100????????????????????????????};
-            bins lb = {32'b100000????????????????????????????};
-            bins sb = {32'b101000????????????????????????????};
-
-            bins j = {32'b000010??????????????????????????????};
-        }
-
-        last_inst_cp: coverpoint last_inst; 
-        curr_inst_cp: coverpoint curr_inst; 
-
-        cross_inst: cross last_inst_cp, curr_inst_cp; // Updated cross
-    endgroup
-
-    function sample();
-        opcodes.sample();
     endfunction
 endclass
-
-
-
-
 
 
 module tb_top (); 
@@ -157,105 +118,175 @@ module tb_top ();
     reg [15:0] switches = 0;
     logic [15:0] leds;
 
+    //vars for disp
+    reg [3:0] digit3, digit2, digit1, digit0;
 
-//generate random instructions and write them to memory
-    initial begin
-        file = $fopen("testbenchmem.dat", "w");
+    //vars for gpiomem
+    reg [7:0] data_out_mem, data_in_mem;
+    reg [8:0] address_mem;
+    reg rw_mem;
 
-        if (file == 0) begin
-            $display("Error: File could not be opened.");
-            $finish;
-        end
+    //vars for core0
+    reg grant_given_cpu0, grant_request_cpu0, rw_cpu0;
+    reg[7:0] data_in_cpu0, data_out_cpu0;
+    reg[8:0] address_cpu0;
 
-        Transactions = new();
-        assert(Transactions.randomize()) else $fatal("Randomization failed");
-        foreach (Transactions.trans_array[i]) begin  
-            $fwrite(file, "%h\n", Transactions.trans_array[i].instruction[31:24]); 
-            $fwrite(file, "%h\n", Transactions.trans_array[i].instruction[23:16]);
-            $fwrite(file, "%h\n", Transactions.trans_array[i].instruction[15:8]);
-            $fwrite(file, "%h\n", Transactions.trans_array[i].instruction[7:0]);            
-        end
-        // Close the file
-        $fclose(file);
-    end
+    //vars for core1
+    reg grant_given_cpu1;
+    reg grant_request_cpu1 = 0;
+    reg rw_cpu1;
+    reg[7:0] data_in_cpu1, data_out_cpu1;
+    reg[8:0] address_cpu1;
 
-    
+
+
+
     initial begin
         clk_100MHz <= '0;
-        forever #(0.5) clk_100MHz <= ~clk_100MHz;
+        forever #(1) clk_100MHz <= ~clk_100MHz;
     end
 
-  
+    int counter = 0;
+    int iteration = 0;
+    int loops;
+    int counterMax = 1_000;
+    int loopMax = 5;
+    always @(posedge  clk_100MHz) begin
+         counter = counter + 1;
+         if(counter == counterMax) begin
+            iteration = 1;
+            loops = loops + 1;
+            $display("loops: %d", loops);
+            counter = 0;
+        end else begin
+            iteration = 0;
+        end
 
-    
+        if (iteration) begin
+            file = $fopen("testbench.dat", "w");
 
-    mips mips_inst
-        (
-            .clk_100MHz     (clk_100MHz),
-            .rst            (rst),
-            .Anode_Activate (Anode_Activate),
-            .LED_out        (LED_out),
-            .buttons        (buttons),
-            .switches       (switches),
-            .leds           (leds)
+            if (file == 0) begin
+                $display("Error: File could not be opened.");
+                $finish;
+            end
+
+            Transactions = new();
+            assert(Transactions.randomize()) else $fatal("Randomization failed");
+            foreach (Transactions.trans_array[i]) begin  
+                $fwrite(file, "%h\n", Transactions.trans_array[i].instruction[31:24]); 
+                $fwrite(file, "%h\n", Transactions.trans_array[i].instruction[23:16]);
+                $fwrite(file, "%h\n", Transactions.trans_array[i].instruction[15:8]);
+                $fwrite(file, "%h\n", Transactions.trans_array[i].instruction[7:0]);            
+            end
+            // Close the file
+            $fclose(file);
+
+            rst <= '1;
+            repeat(2)@(posedge clk_100MHz);
+            rst <= '0;
+            iteration = 0;
+            $display("reset");
+        end
+
+        if(loops >= loopMax) begin
+            $finish;
+        end
+    end
+
+    gpiomem mem_gpio (
+        .clk(clk_100MHz),
+        .rw_select(rw_mem),
+        .reset(rst),
+        .address(address_mem), .data_in(data_in_mem), 
+        .data_out(data_out_mem),
+        .buttons(buttons),
+        .switches(switches),
+        .leds(leds),
+        .digit3(digit3), .digit2(digit2), .digit1(digit1), .digit0(digit0)    
+    );
+
+
+    core #(.pc_start(0)) core0 (
+        .clk(clk_100MHz),
+        .reset(rst),
+        .grant_given(grant_given_cpu0), 
+        .grant_request(grant_request_cpu0),
+        .rw(rw_cpu0),
+        .data_in(data_in_cpu0), 
+        .data_out(data_out_cpu0),
+        .address(address_cpu0)
         );
 
-      // synchronous reset
-    logic srstb;
-    initial begin
-        rst <= '1;
-        repeat(10)@(posedge clk_100MHz);
-        rst <= '0;
+    bus system_bus(
+        .clk(clk_100MHz),
+        .reset(rst),
+
+        .core0_request(grant_request_cpu0),
+        .core0_grant(grant_given_cpu0),
+        .core0_data_in(data_out_cpu0),
+        .core0_data_out(data_in_cpu0),
+        .core0_address(address_cpu0),
+        .core0_rw(rw_cpu0),
+        
+        .core1_request(grant_request_cpu1),
+        .core1_grant(grant_given_cpu1),
+        .core1_data_in(data_out_cpu1),
+        .core1_data_out(data_in_cpu1),
+        .core1_address(address_cpu1),
+        .core1_rw(rw_cpu1),
+        
+        .RAM_address(address_mem),
+        .RAM_data_in(data_in_mem),
+        .RAM_data_out(data_out_mem),
+        .rw(rw_mem)    
+    );
+
+    always @(posedge rst) begin
+        $display("file rewritten");
+        $readmemh("testbench.dat", mem_gpio.RAM);
     end
 
 
-     instruction_transitions core0_instr_cov = new(0);
-    instruction_transitions core1_instr_cov = new(1);
+    class instruction_transitions;
+        bit [31:0] last_inst, curr_inst; // New variables
+
+        covergroup opcodes;
+            inst_reg_opcodes: coverpoint core0.IR {
+            //coverpoint current_inst {
+                bins add = {32'b000000????????????????????100000};
+                bins sub = {32'b000000????????????????????100010};
+                bins and_instr = {32'b000000????????????????????100100};
+                bins or_instr = {32'b000000????????????????????100101};
+                bins slt = {32'b000000????????????????????101010};
+
+                bins addi = {32'b001000??????????????????????????};
+                bins beq = {32'b000100??????????????????????????};
+                bins lb = {32'b100000??????????????????????????};
+                bins sb = {32'b101000??????????????????????????};
+
+                bins j = {32'b000010??????????????????????????};
+            }
+
+            last_inst_cp: coverpoint last_inst; 
+            curr_inst_cp: coverpoint curr_inst; 
+
+            cross_inst: cross last_inst_cp, curr_inst_cp; // Updated cross
+        endgroup
+
+        function sample();
+            opcodes.sample();
+        endfunction
+    endclass
+
+    instruction_transitions core0_instr_cov = new();
+
     always @(posedge clk_100MHz) begin
-        if (mips_inst.core0.state == 4) begin
-            core0_instr_cov.curr_inst = mips_inst.core0.IR;
+        if (core0.state == 4) begin
+            core0_instr_cov.curr_inst = core0.IR;
             core0_instr_cov.sample();
-            core0_instr_cov.last_inst = mips_inst.core0.IR;
-        end
-        if (mips_inst.core1.state == 4) begin
-            core1_instr_cov.curr_inst = mips_inst.core1.IR;
-            core1_instr_cov.sample();
-            core1_instr_cov.last_inst = mips_inst.core1.IR;
+            $display("sample");
+            core0_instr_cov.last_inst = core0.IR;
         end
     end
 
-    task init();
-        buttons    <= '0;
-        switches   <= '0;
-    endtask
-
-    task drive(int iter);
-        for(int it = 0; it < iter; it++) begin
-            buttons    <= '0;
-            switches   <= '0;
-            @(posedge clk);
-        end
-    endtask
-
-    
-
-    initial begin
-        // do something
-
-        init();
-        repeat(10)@(posedge clk);
-
-        drive(20);
-
-        repeat(10)@(posedge clk);
-        $finish;
-    end
-    // // dump wave
-    // initial begin
-    //     $display("random seed : %0d", $unsigned($get_initial_random_seed()));
-    //     if ( $test$plusargs("fsdb") ) begin
-    //         $fsdbDumpfile("tb_top.fsdb");
-    //         $fsdbDumpvars(0, "tb_top", "+mda", "+functions");
-    //     end
-    // end
 endmodule
